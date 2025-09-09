0d5685ac346fc2561f7c0bf7858a24bb72f40a13
Sheikh Saad Sristy <sristy.cse@gmail.com>
2025-09-09 19:34:59 +0600

    priority queue added

--- DIjkstra/Source.cpp
+++ DIjkstra/Source.cpp
@@ -1,88 +1,84 @@
 #include <iostream>
-#include "Source.h"
+#include <queue>
+
 #define INFINITY 1000000
 #define MAX 200
 
 using namespace std;
 
-int V, E;
-
-int G[MAX][MAX];
+int V, E, src;
+vector<pair<int, int>> graph[MAX];
 int dist[MAX];
-int used[MAX];
-int parent[MAX];
 
+priority_queue<
+	pair<int, int>
+> pq;
 
-void ReadVertices()
-{
-	for (int i = 0; i < V; i++) {
-		for (int j = 0; j < V; j++) {
-			G[i][j] = (i == j ? 0 : INFINITY);
-		}
-	}
+void readInputs() {
+	cin >> V >> E;
+	cin >> src;
 }
 
-void ReadEdges() {
+void buildGraph() {
 	for (int i = 0; i < E; i++) {
 		int u, v, w;
 		cin >> u >> v >> w;
-		if (w < G[u][v]) G[u][v] = w;
+		graph[u].push_back({ w,v });
 	}
 }
 
-int minIndexUnused() {
-	int u = -1, best = INFINITY;
-
-	for (int i = 0; i < V; i++) {
-		if (used[i] == 0 && dist[i] < best) {
-			best = dist[i];
-			u = i;
-		}
-	}
-
-	return u;
-}
-
-void Dijkstra(int src) {
+void initialiseDistance() {
 	for (int i = 0; i < V; i++) {
 		dist[i] = INFINITY;
-		used[i] = 0;
-		parent[i] = -1;
 	}
 
 	dist[src] = 0;
+	pq.push({ 0,src });
+}
 
+void Dijkstra() {
+	cout << "pq size " << pq.size() << endl;
+	while (!pq.empty()) {
+		int current_dist = -pq.top().first;
+		int current_node = pq.top().second;
+		pq.pop();
+		if (current_dist > dist[current_node]) continue;
+		
+		for (int i = 0; i < graph[current_node].size(); i++) {
+			int next_node = graph[current_node][i].second;
+			int next_dist = graph[current_node][i].first;
+
+			int new_dist = dist[current_node] + next_dist;
+
+			if (new_dist < dist[next_node]) {
+				dist[next_node] = new_dist;
+				pq.push({ -new_dist,next_node });
+			}
+		}
+	}
+}
+
+void output() {
 	for (int i = 0; i < V; i++) {
-		int u = minIndexUnused();
-		if (u == -1) break;
-		used[u] = 1;
-
-		for (int v = 0; v < V; v++) {
-			if (G[u][v] >= INFINITY) {
-				continue;
-			}
-
-			int cand = dist[u] + G[u][v];
-			if (cand < dist[v]) {
-				dist[v] = cand;
-				parent[v] = u;
-			}
+		if (dist[i] == INFINITY) {
+			cout << "NODE " << i << " unreachable" << endl;
+		}
+		else
+		{
+			cout << "Node " << i << ": " << dist[i] << endl;
 		}
 	}
 }
 
 int main() {
-	cin >> V >> E;
-	ReadVertices();
-	ReadEdges();
-	
-	Dijkstra(0);
-	for (int i = 0; i < V; i++) {
-		if (dist[i] >= INFINITY) cout << -1;
-		else cout << dist[i];
-		if (i + 1 < V) cout << ' ';
-	}
-	cout << "\n";
-	return 0;
-}
+	freopen("input.txt", "r", stdin);
+	cout << "buildGraph..." << endl;
+	readInputs();
+	buildGraph();
+	initialiseDistance();
+	Dijkstra();
 
+	cout << "initialiseDistance..." << endl;
+	cout << "Dijkstra..." << endl;
+	output();
+}
